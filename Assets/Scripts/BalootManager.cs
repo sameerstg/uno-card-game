199ba071509cc0ff5fdbb13ef4e02c1da1e6bbd8
Sameerstg <sameerstg@outlook.com>
2023-03-19 19:13:13 +0500

    init classes

---
+++ Assets/Scripts/BalootManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalootManager : MonoBehaviour
+{
+    public Baloot baloot;
+    private void Start()
+    {
+        baloot = new();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player[] players = new Player[4];
+                
+}
