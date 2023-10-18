7d1e3a0a2943295e4ac43666309507d3b0638dc9
Phoenix <saadanahsan@gmail.com>
2023-10-18 21:34:18 +0500

    Basic Animation Done

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -2,6 +2,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
+using System.Collections;
 
 public class GameUIManager : MonoBehaviour
 {
@@ -10,10 +12,11 @@ public class GameUIManager : MonoBehaviour
     public Slot[] slots;
     public Slot playedCardSlot;
     public GameObject cardPrefab;
+    public GameObject cardBack;
     public Transform deck;
-    
+
     public string nameOfPlayer;
-    
+
     private void Awake()
     {
         _instance = this;
@@ -47,11 +50,11 @@ public class GameUIManager : MonoBehaviour
                 //Debug.Log(texture);
                 GameObject card = Instantiate(cardPrefab, slots[i].cardParent.transform);
                 slots[i].cards.Add(card);
-                               var cardClass = card.AddComponent<BalootCard>();
+                var cardClass = card.AddComponent<BalootCard>();
                 cardClass.cardClass = new(BalootGameManager._instance.cardManager.playerClasses[i].cards[k].house, BalootGameManager._instance.cardManager.playerClasses[i].cards[k].cardName);
                 cardClass.GetComponent<RawImage>().texture = texture;
                 cardClass.cardClass.player = i;
-                
+
             }
         }
 
@@ -66,7 +69,7 @@ public class GameUIManager : MonoBehaviour
             Destroy(item);
         }
         playedCardSlot.cards.Clear();
-        if(BalootGameManager._instance.cardManager.playedCards.Count == 0)
+        if (BalootGameManager._instance.cardManager.playedCards.Count == 0)
         {
             return;
         }
@@ -82,6 +85,7 @@ public class GameUIManager : MonoBehaviour
         cardClass.cardClass = new(BalootGameManager._instance.cardManager.playedCards[^1].house, BalootGameManager._instance.cardManager.playedCards[^1].cardName);
         cardClass.GetComponent<RawImage>().texture = texture;
         cardClass.GetComponent<RawImage>().raycastTarget = false;
+        card.GetComponent<RectTransform>().DOSizeDelta(new Vector2(100, 140), 0f);
     }
     internal void ShowTurn()
     {
@@ -114,7 +118,7 @@ public class GameUIManager : MonoBehaviour
                 slots[i].nameTitle.text += $" Turn";
             }
             int k = 0;
-            CardAnimation(i, k);
+            StartCoroutine(CardAnimation(i, k));
             //for (int k = 0; k < BalootGameManager._instance.cardManager.playerClasses[i].cards.Count; k++)
             //{
             //    //Debug.Log(PlayerManager._instance.players[i].balootPlayerClass.cards[k].house);
@@ -138,11 +142,9 @@ public class GameUIManager : MonoBehaviour
             //    slots[i].cards.Add(card);
             //}
         }
-
-        RefreshPlayedCards();
     }
 
-    void CardAnimation(int i, int k)
+    IEnumerator CardAnimation(int i, int k)
     {
         print("CardAnimation started " + i + " " + k + " " + Time.time);
         Texture2D texture = Instantiate
@@ -150,33 +152,70 @@ public class GameUIManager : MonoBehaviour
                     + $"{BalootGameManager._instance.cardManager.playerClasses[i].cards[k].cardName}")));
         Debug.Log(texture);
         GameObject card = Instantiate(cardPrefab, deck);
+        GameObject carBack = Instantiate(cardBack, card.transform);
         var cardClass = card.AddComponent<BalootCard>();
         cardClass.cardClass = new(BalootGameManager._instance.cardManager.playerClasses[i].cards[k].house, BalootGameManager._instance.cardManager.playerClasses[i].cards[k].cardName);
         cardClass.GetComponent<RawImage>().texture = texture;
         cardClass.cardClass.player = i;
         card.transform.position = deck.position;
+        card.GetComponent<RectTransform>().DOSizeDelta(new Vector2(100, 140), 0f);
         Vector3 targetPosition = slots[i].cardParent.transform.position;
         if (slots[i].cardParent.transform.childCount != 0)
         {
-            targetPosition = slots[i].cardParent.transform.GetChild(slots[i].cardParent.transform.childCount - 1).position;
+            targetPosition += new Vector3(k * 50, 0f, 0f);
         }
-        card.transform.DOMove(targetPosition, 2f).OnComplete(delegate
+        card.transform.DORotate(new Vector3(0f, 90f, 0f), 0.5f, RotateMode.Fast).OnComplete(delegate
+        {
+            carBack.GetComponent<RawImage>().color = Color.clear;
+            card.transform.DORotate(new Vector3(0f, 0f, 0f), 0.5f, RotateMode.Fast);
+        });
+        card.transform.DOMove(targetPosition, 1.5f).OnComplete(delegate
         {
             print("Card Moved: " + card.name);
             card.transform.SetParent(slots[i].cardParent.transform);
             slots[i].cards.Add(card);
-            //slots[i].cardParent.GetComponent<GridLayout>().enabled = false;
-            //slots[i].cardParent.GetComponent<GridLayout>().enabled = true;
-            k++;
 
-            if(k < BalootGameManager._instance.cardManager.playerClasses[i].cards.Count)
-            {
-                CardAnimation(i, k);
-            }
-            else
-            {
-                ShowTurn();
-            }
+            //k++;
+
+            //if (k < BalootGameManager._instance.cardManager.playerClasses[i].cards.Count)
+            //{
+            //    CardAnimation(i, k);
+            //}
+            //else
+            //{
+            //    ShowTurn();
+            //}
         });
+
+        yield return new WaitForSeconds(0.5f);
+
+        k++;
+
+        if (k < BalootGameManager._instance.cardManager.playerClasses[i].cards.Count)
+        {
+            StartCoroutine(CardAnimation(i, k));
+        }
+        else
+        {
+            StartCoroutine(PlayedCardAnimation());
+            yield return new WaitForSeconds(1.5f);
+            ShowTurn();
+        }
+    }
+
+    IEnumerator PlayedCardAnimation()
+    {
+        yield return new WaitForSeconds(1f);
+
+        RefreshPlayedCards();
+        Vector3 targetPosition = playedCardSlot.cards[^1].transform.position;
+        GameObject carBack = Instantiate(cardBack, playedCardSlot.cards[^1].transform);
+        playedCardSlot.cards[^1].transform.DORotate(new Vector3(0f, 90f, 0f), 0.5f, RotateMode.Fast).OnComplete(delegate
+        {
+            carBack.GetComponent<RawImage>().color = Color.clear;
+            playedCardSlot.cards[^1].transform.DORotate(new Vector3(0f, 0f, 0f), 0.5f, RotateMode.Fast);
+        });
+        playedCardSlot.cards[^1].transform.position = deck.position;
+        playedCardSlot.cards[^1].transform.DOMove(targetPosition, 1.5f);
     }
 }
